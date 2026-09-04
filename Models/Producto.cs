cd17d4e040b99e9406c48744546aab79819772b2
Juncal Serrano <juncalserrano04@gmail.com>
2026-09-04 16:40:08 -0600

    modifique la clase producto por lo que necesitamos

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -3,8 +3,8 @@
     public class Producto
     {
         public string Id { get; set; } = string.Empty;
-        public string Nombre { get; set; } = string.Empty;
-        public string Descripcion { get; set; } = string.Empty;
+        public string Modelo { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
         public decimal Precio { get; set; }
         public string ImagenUrl { get; set; } = string.Empty;
     }
